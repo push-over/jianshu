36e98664a4eed0987b781ec89edf6166890a2122
Zane <push_over@163.com>
2018-12-27 23:21:32 +0800

    集成redux 重构搜索框

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -50,18 +50,6 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
     position: relative;
     float: left; 
-    .slide-enter {
-        transition: all .3s ease-out;
-    }
-    .slide-enter-active {
-        width: 240px;
-    }
-    .slide-exit {
-        transition: all .3s ease-out;
-    }
-    .slide-exit-active {
-        width: 160px;
-    }
     .iconfont {
         position: absolute;
         right: 5px;
@@ -100,6 +88,18 @@ export const NavSearch = styled.input.attrs({
     }
     &.focused {
         width: 240px;
+    }
+    &.slide-enter {
+        transition: all .3s ease-out;
+    }
+    &.slide-enter-active {
+        width: 240px;
+    }
+    &.slide-exit {
+        transition: all .3s ease-out;
+    }
+    &.slide-exit-active {
+        width: 160px;
     }
 `;
 
