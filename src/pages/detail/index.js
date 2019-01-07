8611083c79ec6ecf829f941fb500278f744f73fb
Zane <push_over@163.com>
2019-01-07 17:26:53 +0800

    文章详情底部完成部分

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -7,15 +7,25 @@ import {
   DetailAuthorInfo,
   DetailAuthorMeta,
   DetailContent,
+  SupportAuthor,
+  SupportPay,
+  Supporter,
+  DetailFoot,
+  FollowDetail,
+  FollowInfo,
+  Signature
 } from './style';
 
 class Detail extends PureComponent {
   render() {
     return (
       <DetailWrapper>
+        {/* 文章详情 */}
         <DetailTitle>
           杨超越获2018年度演艺人物，她演了啥？ 一条锦鲤吗？
         </DetailTitle>
+
+        {/* 文章作者信息 */}
         <DetailAuthor>
           <img className='avatar' src='//upload.jianshu.io/users/upload_avatars/147245/f6d3f69dec9e.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96' alt='' />
           <DetailAuthorInfo>
@@ -30,12 +40,44 @@ class Detail extends PureComponent {
             </DetailAuthorMeta>
           </DetailAuthorInfo>
         </DetailAuthor>
+
+        {/* 文章内容 */}
         <DetailContent>
           <img src='//upload-images.jianshu.io/upload_images/147245-e4ded9411586fcbe.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/600/format/webp' alt='' />
           <p>杨超越的火，绝对是娱乐圈的一大玄学，在2018年还剩下最后十天的时候，比“杨超越火”更玄的事情出现了，那就是——杨超越被《中国新闻周刊》评为“2018影响中国年度人物”。</p>
           <img src='//upload-images.jianshu.io/upload_images/147245-5cf552a03ea02faa?imageMogr2/auto-orient/strip%7CimageView2/2/w/588/format/webp' alt='' />
           <p>故宫博物院院长单霁翔被评为“年度文化人物”，中国乒乓球协会主席刘国梁被评为“年度体育人物”，《我不是药神》导演文牧野被评为“年度导演”，还有火箭少女101成员杨超越被评为“年度演艺人物”。</p>
         </DetailContent>
+
+        {/* 赞赏支持 */}
+        <SupportAuthor>
+          <p>欢迎赞赏...</p>
+          <SupportPay>赞赏支持</SupportPay>
+          <Supporter></Supporter>
+        </SupportAuthor>
+
+        <DetailFoot>
+          <i className="iconfont">&#xe68f;</i>
+          <span>前端知识点</span>
+          <span className="copyright">© 著作权归作者所有</span>
+          <span className="modal-wrap">举报文章</span>
+        </DetailFoot>
+
+        {/* 文章底部作者信息 */}
+        <FollowDetail>
+          <FollowInfo>
+            <img className="avatar" src='//upload.jianshu.io/users/upload_avatars/4041074/79878f31-c8ae-4087-880c-bc2185837f1d.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96' alt=''/>
+            <span className="success">
+              <i className="iconfont">&#xe606;</i>
+              关注
+            </span>
+            <span className="title">ywyan</span>
+            <p>写了 113086 字，被 162 人关注，获得了 329 个喜欢</p>
+          </FollowInfo>
+          <Signature>
+          天道酬勤，思者常新；博观约取，厚积薄发；心如止水，气贯长虹；淡漠明志，宁静致远。微信公众号：“前端技术博文”，欢迎关注！
+          </Signature>
+        </FollowDetail>
       </DetailWrapper>
     )
   }
--- src/pages/detail/style.js
+++ src/pages/detail/style.js
@@ -69,4 +69,124 @@ export const DetailContent = styled.div `
     font-size: 16px;
     font-weight: 400;
     line-height: 1.7;
+    margin: 0 0 25px;
+`;
+
+export const SupportAuthor = styled.div `
+    padding: 20px 0;
+    text-align: center;
+    clear: both;
+    box-sizing: border-box;
+    p {
+        padding: 0 30px;
+        margin-bottom: 20px;
+        font-size: 17px;
+        font-weight: 700;
+        color: #969696;
+    }
+`;
+
+export const SupportPay = styled.div `
+    display: inline-block;
+    margin-bottom: 20px;
+    padding: 11px 25px;
+    font-size: 16px;
+    color: #fff;
+    background: #ea6f5a;
+    border-radius: 20px;
+    box-sizing: border-box;
+    cursor: pointer;
+    border: 1px solid transparent;
+`;
+
+export const Supporter = styled.div `
+    height: 50px;
+`;
+
+export const DetailFoot = styled.div `
+    margin-bottom: 30px;
+    font-size: 12px;
+    color: #c8c8c8;
+    cursor: pointer;
+    .iconfont {
+        margin-right: 2px;
+        font-size: 15px;
+    }
+    .copyright {
+        display: inline-block;
+        float: right;
+    }
+    .modal-wrap {
+        float: right;
+        margin-right: 20px;
+    }
+`;
+
+export const FollowDetail = styled.div `
+    padding: 20px;
+    background-color: hsla(0,0%,71%,.1);
+    border: 1px solid #e1e1e1;
+    border-radius: 4px;
+    font-size: 12px;
+    box-sizing: border-box;
+`;
+
+export const FollowInfo = styled.div `
+    .avatar {
+        float: left;
+        margin-right: 10px;
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        border: 1px solid #ddd;
+        cursor: pointer;
+        display: inline-block;
+    }
+    .success {
+        display: inline-block;
+        float: right;
+        margin-top: 9px;
+        padding: 11px 0;
+        width: 100px;
+        font-size: 16px;
+        border-color: #42c02e;
+        background: #42c02e;
+        border-radius: 40px;
+        color: #fff;
+        cursor: pointer;
+        border: 1px solid transparent;
+        text-align: center;
+        .iconfont {
+            margin-right: 3px;
+        }
+    }
+    .success:hover {
+        background: #3db922;
+    }
+
+    .title {
+        color: #2f2f2f;
+        margin-right: 3px;
+        font-size: 17px;
+        line-height: 1.8;
+        cursor: pointer;
+        vertical-align: middle;
+    }
+
+    p {
+        margin: 0 0 10px;
+        color: #969696;
+    }
+`;
+
+export const Signature = styled.div `
+    margin-top: 20px;
+    padding-top: 20px;
+    border-top: 1px solid #e1e1e1;
+    color: #969696;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    box-sizing: border-box;
+}
 `;
